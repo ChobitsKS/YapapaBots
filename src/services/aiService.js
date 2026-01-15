da05f483677c465c01ae3e5a4e9bff96f9598212
ChobitsKS <chobits.ks@gmail.com>
2026-01-15 10:01:24 +0700

    Add files via upload

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.0-flash",
     generationConfig: {
         maxOutputTokens: 500, // จำกัด Output
         temperature: 0.7
